66d1f85c7b43a23a9ff158d812dfe4262f262bd0
AtanasovAlexandr <mephistosaa@gmail.com>
2020-02-03 21:01:09 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div>
+      <p>Тут будет:</p>
+      <ul>
+        <li>Регистрация, авторизация</li>
+        <li>Шапка сайта</li>
+        <li>Карточки с наполнением</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
