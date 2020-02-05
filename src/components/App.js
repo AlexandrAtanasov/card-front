a75cd2196c966b6280b957bbf1222a6280d71162
AtanasovAlexandr <mephistosaa@gmail.com>
2020-02-05 22:28:37 +0300

    #001 - let's go

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function App() {
   return (
     <div className='container'>
-      <div className="jumbotron">
-        <h1 className="display-4">Тут будет форма авторизации и регистрации</h1>
-      </div>
+
     </div>
   );
 }
