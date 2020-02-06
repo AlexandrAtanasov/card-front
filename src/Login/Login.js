e91823e7ab823058828e344ff4a6755e03f1d7c6
AtanasovAlexandr <mephistosaa@gmail.com>
2020-02-07 00:27:28 +0300

    email and password validation. form with two password fields

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+// import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 function Copyright() {
   return (
@@ -49,7 +50,47 @@ const useStyles = makeStyles(theme => ({
 
 export default function Login() {
   const classes = useStyles();
-  const [isSignUp, setSign] = useState(0);
+  // const [isSignUp, setSign] = useState(0);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  let inputValidatorForm = useRef(null);
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+    // setSubmitted({ submitted: true }, () => {
+    //     setTimeout(() => setSubmitted({ submitted: false }), 3000);
+    // });
+  };
+  const handleInputEmail = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+  };
+  const handleInputPassword = (e) => {
+    setPassword(e.target.value);
+    if (e.target.name === 'password') {
+      inputValidatorForm.isFormValid(false);
+    }
+    setPassword(e.target.value);
+  };
+  const handleInputRepeatPassword = (e) => {
+    setRepeatPassword(e.target.value);
+    if (e.target.name === 'repeatPassword') {
+      inputValidatorForm.isFormValid(false);
+    }
+    setRepeatPassword(e.target.value);
+  };
+  useEffect(() => {
+    // custom rule will have name 'isPasswordMatch'
+    ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
+      if (value !== password) {
+        return false;
+      }
+      return true;
+    });
+  });
 
   return (
     <Container component="main" maxWidth="xs">
@@ -61,42 +102,78 @@ export default function Login() {
         <Typography component="h1" variant="h5">
           Sign up | Регистрация
         </Typography>
-        <form className={classes.form} noValidate>
+        <ValidatorForm
+          className={classes.form} 
+          onSubmit={handleSubmit}
+          noValidate
+          ref={r => (inputValidatorForm = r)}
+        >
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
+              <TextValidator
                 name="email"
+                id="email"
                 autoComplete="email"
+                label="Email"
+                variant="outlined"
+                value={email}
+                onChange={handleInputEmail}
+                validators={['required', 'isEmail']}
+                errorMessages={['this field is required', 'email is not valid | введён некорректный адрес']}
+                fullWidth
                 autoFocus
+                required
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
+              <TextValidator
                 name="password"
                 label="Password"
                 type="password"
                 id="password"
+                variant="outlined"
                 autoComplete="current-password"
+                onChange={handleInputPassword}
+                validators={['required']}
+                errorMessages={['this field is required']}
+                value={password}
+                required
+                fullWidth
               />
+            </Grid>
+            <Grid item xs={12}>
+              <TextValidator
+                name="repeatPassword"
+                label="Password"
+                type="password"
+                id="repeatPassword"
+                variant="outlined"
+                autoComplete="current-password"
+                onChange={handleInputRepeatPassword}
+                validators={['isPasswordMatch', 'required']}
+                errorMessages={['password mismatch', 'this field is required']}
+                value={repeatPassword}
+                required
+                fullWidth
+                />
             </Grid>
           </Grid>
           <Button
             type="submit"
-            fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            fullWidth
+            // disabled={submitted}
+            // disabled Ругается
           >
-            Sign Up | Зарегистрироваться
+            {
+              (submitted && 'Your form is submitted!')
+              || 
+              (!submitted && 'Sign Up | Зарегистрироваться')
+            }
           </Button>
+
           <Grid container justify="flex-end">
             <Grid item>
               <Link href="#" variant="body2">
@@ -106,11 +183,12 @@ export default function Login() {
               </Link>
             </Grid>
           </Grid>
-        </form>
+        </ValidatorForm>
       </div>
       <Box mt={5}>
         <Copyright />
       </Box>
     </Container>
   );
+
 }
