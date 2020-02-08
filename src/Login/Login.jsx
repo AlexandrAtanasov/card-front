c10637d25349a544033513f8b26b5992bbe77756
AtanasovAlexandr <mephistosaa@gmail.com>
2020-02-08 20:52:50 +0300

    js -> jsx; uninst bootstrap; del trash

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -7,25 +7,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 // import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+// import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="#">
-        This Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -185,9 +172,6 @@ export default function Login() {
           </Grid>
         </ValidatorForm>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </Container>
   );
 
